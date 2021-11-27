d30138b22ee5cb58b753fc52444f545c7fd128e4
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2021-11-27 13:18:52 -0200

    Programação Orientada a Objetos

---
+++ js/index.js
@@ -0,0 +1,24 @@
+class Produto {
+
+    constructor(){
+
+        this.id = 0;
+        this.nomeProduto = "";
+        this.valor = 0;
+
+    }
+
+    adicionar(){
+        alert("Adicionar um produto");
+        //
+    }
+
+    excluir(){
+        alert('Item deletado')
+
+
+    }
+
+}
+
+var produto = new Produto()
