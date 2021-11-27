6aa3ed11dee013b9c86dc37f406bf23d968553e9
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2021-11-27 16:09:50 -0200

    Como Adicionar e Manipular Objetos no Array

--- js/index.js
+++ js/index.js
@@ -2,19 +2,54 @@ class Produto {
 
     constructor(){
 
-        this.id = 0;
-        this.nomeProduto = "";
-        this.valor = 0;
+        this.id = 1;
+        this.arrayProdutos = [];
+        
+    }
+
+    salvar(){
+        let produto = this.lerDados();
+        this.validaCampos(produto)
+        if(this.validaCampos(produto)){
+            this.adicionar(produto)
+        }
+            console.log(this.arrayProdutos)
+    }
+    adicionar(produto){
+        this.arrayProdutos.push(produto);
+        this.id++
 
     }
 
-    adicionar(){
-        alert("Adicionar um produto");
-        //
+
+    lerDados(){
+        let produto = {}
+            produto.id = this.id;
+            produto.nomeProduto = document.getElementById('produto').value
+            produto.preço = document.getElementById('preço').value
+        
+            return produto;
+        
     }
 
-    excluir(){
-        alert('Item deletado')
+    validaCampos(produto){
+        let msg = "";
+        if(produto.nomeProduto == ""){
+            msg += "Informe o nome do produto \n"
+        }
+        if(produto.preço == ""){
+            msg += "Informe o preço do produto \n"
+        }
+        if(msg != ""){
+            alert(msg);
+            return false;
+        }
+        return true;
+
+    }
+
+    cancelar(){
+        
 
 
     }
