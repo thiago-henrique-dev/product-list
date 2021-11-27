06736239de054bf0374e706c6a420086fa7e0308
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2021-11-27 18:01:26 -0200

    Como Listar Dados do Array em Tabela HTML

--- js/index.js
+++ js/index.js
@@ -13,7 +13,39 @@ class Produto {
         if(this.validaCampos(produto)){
             this.adicionar(produto)
         }
-            console.log(this.arrayProdutos)
+           this.listaTabela();
+           this.cancelar();
+    }
+    listaTabela(){
+        let tbody = document.getElementById('tbody')
+        tbody.innerText = '';
+        for(let i = 0; i < this.arrayProdutos.length; i++){
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_produto = tr.insertCell();
+            let td_valor = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+            td_id.innerText = this.arrayProdutos[i].id
+            td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+            td_valor.innerText = this.arrayProdutos[i].preço
+            
+            td_id.classList.add('center')
+
+            let imgEdit = document.createElement('img');
+            let imgDelete = document.createElement('img')
+            imgEdit.src = "img/baseline_delete_black_24dp.png"
+            imgDelete.src = 'img/outline_edit_black_24dp.png'
+            
+
+
+            td_acoes.appendChild(imgEdit)
+            td_acoes.appendChild(imgDelete)
+
+        }
+
+
     }
     adicionar(produto){
         this.arrayProdutos.push(produto);
@@ -49,6 +81,8 @@ class Produto {
     }
 
     cancelar(){
+        document.getElementById('produto').value = '';
+        document.getElementById('preço').value = '';
         
 
 
